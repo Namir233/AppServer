f9199280529e785b4c00e91dd877544e6abee60b
王振旺 <wangzhenwang@qiushibaike.com>
2017-06-30 16:35:07 +0800

    版本信息改版

--- Sources/main.swift
+++ Sources/main.swift
@@ -12,26 +12,27 @@ import PerfectHTTP
 import PerfectHTTPServer
 import PerfectMustache
 
-var version: String? = nil
+var ipaInfo: [String: Any]? = nil
+let workDir: String = File("~/server/AppServer/").realPath
 
 var routes = Routes()
 routes.add(method: .get, uri: "/") { (request, response) in
     request.path = "/index.html"
-    StaticFileHandler(documentRoot: "./Static").handleRequest(request: request, response: response)
+    StaticFileHandler(documentRoot: workDir + "Static").handleRequest(request: request, response: response)
 }
 
 routes.add(method: .get, uri: "/res/**") { (request, response) in
     let path = request.path
     let index = path.index(path.startIndex, offsetBy: 4)
     request.path = path.substring(from: index)
-    StaticFileHandler(documentRoot: "./Resource").handleRequest(request: request, response: response)
+    StaticFileHandler(documentRoot: workDir + "Resource").handleRequest(request: request, response: response)
 }
 
 routes.add(method: .get, uri: "/files/**") { (request, response) in
     let path = request.path
     let index = path.index(path.startIndex, offsetBy: 6)
     request.path = path.substring(from: index)
-    StaticFileHandler(documentRoot: "./Static").handleRequest(request: request, response: response)
+    StaticFileHandler(documentRoot: workDir + "Static").handleRequest(request: request, response: response)
 }
 
 func runCommand(launchPath: String, arguments: [String]) -> String {
@@ -48,17 +49,21 @@ func runCommand(launchPath: String, arguments: [String]) -> String {
     return String(data: data, encoding: .utf8)!
 }
 
-func loadIPAVersion() -> String {
-    if version == nil {
-        version = runCommand(launchPath: "/usr/bin/python", arguments: ["./Static/Wolf/ipa_plist.py", "./Static/Wolf/WolfManKill.ipa"])
+func loadIPAInfo() -> [String: Any] {
+    if ipaInfo == nil {
+        let result = runCommand(launchPath: "/usr/bin/python", arguments: [workDir + "Static/Wolf/ipa_plist.py", workDir + "Static/Wolf/WolfManKill.ipa"])
+        let data = result.data(using: .utf8)!
+        
+        let jsonObj = try? JSONSerialization.jsonObject(with: data, options: .mutableContainers)
+        ipaInfo = jsonObj != nil ? jsonObj! as? [String: Any] : nil
     }
-    return version ?? ""
+    return ipaInfo ?? [:]
 }
 
 struct WolfHandler: MustachePageHandler {
     func extendValuesForResponse(context contxt: MustacheWebEvaluationContext, collector: MustacheEvaluationOutputCollector) {
         var values = MustacheEvaluationContext.MapType()
-        values["version"] = loadIPAVersion()
+        values["version"] = loadIPAInfo()["CFBundleVersion"] ?? "?.?.?"
         contxt.extendValues(with: values)
         do {
             try contxt.requestCompleted(withCollector: collector)
@@ -72,19 +77,25 @@ struct WolfHandler: MustachePageHandler {
 }
 
 let wolfIndexHander = { (request, response) in
-    mustacheRequest(request: request, response: response, handler: WolfHandler(), templatePath: "./Mustache/wolf.html")
+    mustacheRequest(request: request, response: response, handler: WolfHandler(), templatePath: workDir + "Mustache/wolf.html")
 }
 
 routes.add(method: .get, uri: "/Wolf", handler: wolfIndexHander)
 
+routes.add(method: .get, uri: "/Wolf/api/update") { (request, response) in
+    response.setHeader(.contentType, value: "text/json")
+    response.appendBody(string: "{\"version\":\(loadIPAInfo()["CFBundleVersion"] ?? "?.?.?")}")
+    response.completed()
+}
+
 routes.add(method: .get, uri: "/Wolf/upload") { (request, response) in
     request.path = "/upload.html"
-    StaticFileHandler(documentRoot: "./Static").handleRequest(request: request, response: response)
+    StaticFileHandler(documentRoot: workDir + "Static").handleRequest(request: request, response: response)
 }
 
 routes.add(method: .post, uri: "/Wolf/upload") { (request, response) in
     if let uploads = request.postFileUploads, let upload = uploads.first {
-        let ipaPath = "./Static/Wolf/WolfManKill.ipa"
+        let ipaPath = workDir + "Static/Wolf/WolfManKill.ipa"
         let ipaFile = File(ipaPath)
         if ipaFile.exists {
             ipaFile.delete()
@@ -96,7 +107,7 @@ routes.add(method: .post, uri: "/Wolf/upload") { (request, response) in
         } catch {
             print(error)
         }
-        version = nil
+        ipaInfo = nil
         response.setHeader(.contentType, value: "text/html")
         response.appendBody(string: "{}")
         response.completed()
@@ -107,7 +118,7 @@ routes.add(method: .get, uri: "/Wolf/**") { (request, response) in
     if request.path == "/Wolf/" {
         wolfIndexHander(request, response)
     } else {
-        StaticFileHandler(documentRoot: "./Static").handleRequest(request: request, response: response)
+        StaticFileHandler(documentRoot: workDir + "Static").handleRequest(request: request, response: response)
     }
 }
 
